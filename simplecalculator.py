2b7d90a29bf3c3c66eb3bda13e8191696da8c9a3
aicha chadli <aicha.chadli@laplateforme.io>
2025-01-15 13:14:01 +0100

    Add try-except block

--- simplecalculator.py
+++ simplecalculator.py
@@ -19,19 +19,23 @@ print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 
+try:
 
-choice = input("Enter choice 1/2/3/4 :")
+  choice = input("Enter choice 1/2/3/4 :")
 
-num1= float(input("Enter first number :"))
-num2= float(input("Enter second number :"))
+  num1= float(input("Enter first number :"))
+  num2= float(input("Enter second number :"))
 
-if choice == '1':
-  print(num1, "+", num2, "=", add(num1, num2))
-elif choice == '2':
-  print(num1, "-", num2, "=", subtract(num1, num2))
-elif choice == '3':
-  print(num1, "*", num2, "=", multiply(num1, num2))
-elif choice == '4':
-  print(num1, "/", num2, "=", divide(num1, num2))
-else:
-  print("Invalid input")
+  if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+  elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+  elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+  elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+  else:
+    print("Invalid input")
+
+except ValueError:
+  print("Error: Invalid input. Please enter valid number.")
